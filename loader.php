348fa198f56a864fc782db989355a7e0a83d4075
Ben Dusinberre <ben@dq5studios.com>
2019-03-06 21:55:55 -0600

    Output in md format, process file doc, property doc

--- loader.php
+++ loader.php
@@ -18,12 +18,14 @@ spl_autoload_register(
         include_once $class;
     }
 );
+require_once "Util/Tokens.php";
 
 
 $class_list = get_declared_classes();
 $sniff_list = glob("Standards/*/Sniffs/*/*.php");
 $doc_list = glob("Standards/*/Docs/*/*.xml");
 
+$sniffs = [];
 foreach ($sniff_list as $filename) {
     include_once $filename;
     $class_check = get_declared_classes();
@@ -31,9 +33,72 @@ foreach ($sniff_list as $filename) {
     $sniff_class = array_pop($sniff_class);
 
     $reflect = new ReflectionClass($sniff_class);
+    $def_vals = $reflect->getDefaultProperties();
     $param_list = $reflect->getProperties(ReflectionProperty::IS_PUBLIC);
+    $props = [];
+    foreach ($param_list as $prop) {
+        $ref = new ReflectionProperty($sniff_class, $prop->getName());
+        if ($prop->getName() == "supportedTokenizers") {
+            // Don't care to list
+            continue;
+        }
+        $doc = $prop->getDocComment();
+        preg_match("/\*\*\s+\*\s+([^@]*)(\*\s+@|\*\/)/sm", $doc, $matched);
+        $desc = "Undocumented";
+        if (!empty($matched) && count($matched) > 1) {
+            $desc = preg_replace("/^\s+\*\s?|\s+$/m", "", $matched[1]);
+            $desc = trim(str_replace("\n", " ", $desc));
+        }
+        preg_match("/@var\s+(\S+)(\s+([^*]*))?\s*$/m", $doc, $matched);
+        if (!empty($matched) && (count($matched) === 2 || count($matched) === 4)) {
+            $subdesc = trim(isset($matched[3]) ? $matched[3] : "");
+            if (!empty($subdesc)) {
+                $desc .= " ({$subdesc})";
+            }
+            $type = trim($matched[1]);
+        }
+        $props[] = [
+            "name" => $prop->getName(),
+            "desc" => $desc,
+            "default" => $def_vals[$prop->getName()],
+            "type" => $type
+        ];
+    }
 
-    echo $sniff_class, PHP_EOL;
-    // var_dump($param_list);
-    // exit;
+    $doc = file_get_contents($filename);
+    preg_match("/\*\*\s+\*\s+([^@]*)(\*\s+@|\*\/)/sm", $doc, $matched);
+    $desc = "Undocumented";
+    if (!empty($matched) && count($matched) > 1) {
+        $desc = preg_replace("/^\s+\*\s?|\s+$/m", "", $matched[1]);
+        $desc = trim(str_replace("\n\n", "\n", $desc));
+    }
+
+    $parts = explode("\\", $sniff_class);
+    $parts[5] = str_replace("Sniff", "", $parts[5]);
+    $ref = "{$parts[2]}.{$parts[4]}.{$parts[5]}";
+    $sniffs[] = [
+        "name" => $ref,
+        "desc" => $desc,
+        "opts" => $props
+    ];
+}
+
+// var_dump($sniffs[14]);
+
+foreach ($sniffs as $j => $sniff) {
+    echo "## {$sniff["name"]}\n";
+    $sniff["desc"] = str_replace(["<code>", "</code>"], ["```php", "```"], $sniff["desc"]);
+    echo "{$sniff["desc"]}\n";
+    if (!empty($sniff["opts"])) {
+        echo "> ### Options\n";
+        $c = count($sniff["opts"]);
+        foreach ($sniff["opts"] as $i => $opt) {
+            echo "> `{$opt["name"]}` _{$opt["type"]}_\\\n";
+            echo "> {$opt["desc"]}\n";
+            if ($i < ($c - 1)) {
+                echo ">\n";
+            }
+        }
+    }
+    echo "\n";
 }
