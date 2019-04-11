4af23b234005accd5ea769f5f2890aa26e07bce5
Ben Dusinberre <ben@dq5studios.com>
2019-04-10 20:07:29 -0500

    Add html output

--- loader.php
+++ loader.php
@@ -15,15 +15,15 @@ spl_autoload_register(
         $class = str_replace("PHP_CodeSniffer", ".", $class);
         $class .= ".php";
         $class = str_replace("\\", "/", $class);
-        include_once $class;
+        include_once "../PHP_CodeSniffer/src/{$class}";
     }
 );
-require_once "Util/Tokens.php";
+require_once "../PHP_CodeSniffer/src/Util/Tokens.php";
 
 
 $class_list = get_declared_classes();
-$sniff_list = glob("Standards/*/Sniffs/*/*.php");
-$doc_list = glob("Standards/*/Docs/*/*.xml");
+$sniff_list = glob("../PHP_CodeSniffer/src/Standards/*/Sniffs/*/*.php");
+$doc_list = glob("../PHP_CodeSniffer/src/Standards/*/Docs/*/*.xml");
 
 $sniffs = [];
 foreach ($sniff_list as $filename) {
@@ -120,29 +120,106 @@ foreach ($sniff_list as $filename) {
     ];
 }
 
-// var_dump($sniffs[16]);
+// var_dump($sniffs[0]);
 // exit;
 
-foreach ($sniffs as $j => $sniff) {
-    echo "## {$sniff["name"]}\n";
-    $sniff["desc"] = str_replace(["<code>", "</code>"], ["```php", "```"], $sniff["desc"]);
-    echo "{$sniff["desc"]}\n";
-    if (!empty($sniff["opts"])) {
-        echo "> ### Options\n";
-        $c = count($sniff["opts"]);
-        foreach ($sniff["opts"] as $i => $opt) {
-            echo "> `{$opt["name"]}` _{$opt["type"]}_\\\n";
-            echo "> {$opt["desc"]}\n";
-            if ($i < ($c - 1)) {
-                echo ">\n";
-            }
-        }
-    }
+// foreach ($sniffs as $sniff) {
+//     echo "## {$sniff["name"]}\n";
+//     $sniff["desc"] = str_replace(["<code>", "</code>"], ["```php", "```"], $sniff["desc"]);
+//     echo "{$sniff["desc"]}\n";
+//     if (!empty($sniff["opts"])) {
+//         echo "> ### Options\n";
+//         $c = count($sniff["opts"]);
+//         foreach ($sniff["opts"] as $i => $opt) {
+//             echo "> `{$opt["name"]}` _{$opt["type"]}_\\\n";
+//             echo "> {$opt["desc"]}\n";
+//             if ($i < ($c - 1)) {
+//                 echo ">\n";
+//             }
+//         }
+//     }
+//     if (!empty($sniff["sniffs"])) {
+//         echo "### Additional Sniffs\n";
+//         foreach ($sniff["sniffs"] as $sub) {
+//             echo "- {$sub}\n";
+//         }
+//     }
+//     echo "\n";
+// }
+
+echo <<<HTML
+<!doctype html>
+<html lang="en">
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="phpcs.css">
+        <title>Smashing</title>
+    </head>
+    <body>
+        <div class="container">
+            <form>\n
+HTML;
+foreach ($sniffs as $sniff) {
+    $sniff["desc"] = str_replace(["<code>", "</code>"], ["<pre><code>", "</code></pre>"], $sniff["desc"]);
+    echo <<<HTML
+    <div class="form-group">
+        <div class="col-sm-6">
+            <h4>
+                <div class="custom-control custom-switch">
+                    <input class="custom-control-input" type="checkbox" id="{$sniff["name"]}">
+                    <label class="custom-control-label" for="{$sniff["name"]}">{$sniff["name"]}</label>
+                </div>
+            </h4>
+            <p>{$sniff["desc"]}</p>
+            <input class="hider" type="radio" name="{$sniff["name"]}" checked hidden>
+            <dl>\n
+HTML;
     if (!empty($sniff["sniffs"])) {
-        echo "### Additional Sniffs\n";
+        echo <<<HTML
+                <dd class="form-group row">
+                    <div class="col-sm-2">Rules</div>
+                    <div class="col-sm-10">\n
+HTML;
         foreach ($sniff["sniffs"] as $sub) {
-            echo "- {$sub}\n";
+            echo <<<HTML
+            <div class="custom-control custom-switch">
+                <input class="custom-control-input" type="checkbox" id="{$sniff["name"]}.{$sub}">
+                <label class="custom-control-label" for="{$sniff["name"]}.{$sub}">{$sub}</label>
+            </div>\n
+HTML;
+        }
+        echo <<<HTML
+                </dd>\n
+HTML;
+    }
+    if (!empty($sniff["opts"])) {
+        foreach ($sniff["opts"] as $opt) {
+            $type = "text";
+            if (in_array($opt["type"], ["int", "integer"])) {
+                $type = "number";
+            }
+            echo <<<HTML
+                    <dt class="form-group row">
+                        <span class="col-sm">{$opt["desc"]}</span>
+                    </dt>
+                    <dd class="form-group row">
+                        <label class="col-sm-3 col-form-label">{$opt["name"]}</label>
+                        <div class="col-sm-9">
+                            <input type="{$type}" class="form-control">
+                        </div>
+                    </dd>\n
+HTML;
         }
     }
-    echo "\n";
+    echo <<<HTML
+            </dl>
+        </div>
+    </div>\n
+HTML;
 }
+echo <<<HTML
+            </form>
+        </div>
+    </body>
+</html>
+HTML;
